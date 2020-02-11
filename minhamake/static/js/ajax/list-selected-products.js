39d7ae53a7be17fe229deffcf05eb8f786820150
mike <mike@shoppingonlinearacaju.com.br>
2020-02-11 01:47:34 -0300

    :sparkles: Funcionalidade para persistir produtos selecionados pelo usuário

---
+++ minhamake/static/js/ajax/list-selected-products.js
@@ -0,0 +1,46 @@
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+var csrftoken = getCookie('csrftoken');
+
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+$.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        }
+    }
+});
+
+function addSelectedProduct(){
+
+    var product = $('select[name=product]');
+    var shade = $('select[name=shade]');
+    
+    request_url = "user/product/add"
+    if(product){
+        $.ajax({
+            type: 'POST', 
+            url: request_url,
+            data: {
+                product: $('select[name=product]').val(), 
+                shade: $('select[name=shade]').val()
+            },
+        })
+    }
+};
